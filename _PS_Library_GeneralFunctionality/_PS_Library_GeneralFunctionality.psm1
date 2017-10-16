6eaf7cc743a1ed839004031d7a37042ded28b788
kevmaitland <iamkevmaitland@gmail.com>
2017-10-16 09:50:46 +0100

    updated convertTo-localisedSecureString

--- _PS_Library_GeneralFunctionality/_PS_Library_GeneralFunctionality.psm1
+++ _PS_Library_GeneralFunctionality/_PS_Library_GeneralFunctionality.psm1
@@ -9,8 +9,8 @@
     $output
     }
 
-function convert-toLocalisedSecureString($plainText){
+function convertTo-localisedSecureString($plainText){
     if ($(Get-Module).Name -notcontains "_PS_Library_Forms"){Import-Module _PS_Library_Forms}
     if (!$plainText){$plainText = form-captureText -formTitle "PlainText" -formText "Enter the plain text to be converted to a secure string" -sizeX 300 -sizeY 200}
-    ConvertTo-SecureString $plainText -AsPlainText -Force
+    ConvertTo-SecureString $plainText -AsPlainText -Force | ConvertFrom-SecureString
     }
