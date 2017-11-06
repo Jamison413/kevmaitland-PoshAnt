dac14c597a7dc2425bbbc90ed27e920391b3186c
kevmaitland <iamkevmaitland@gmail.com>
2017-11-06 09:11:38 +0000

    manage-SpoSites fix& Shared Mailbox management
    manage-spoSites: Fixed issue where newly created SPO Groups were not having the correct Owner set. Changes made to _CSOM_Library-SPO too.
    
    Added new-sharedMailbox functionality.
    
    Added _PS_Library_Registry Module for tinkering with registry settings via PS

---
+++ _PS_Library_Registry/_PS_Library_Registry.psm1
@@ -0,0 +1,15 @@
+﻿function remove-regLeafKeyMatchingString([Microsoft.Win32.RegistryKey]$branchKey,[string]$regexExpressionString,[boolean]$areYouSure){
+    $branchKey | Get-ItemProperty | Get-Member -MemberType Properties | %{
+        if($_.Name -inotmatch "^PS"-and $_.Name -imatch $regexExpressionString){
+            if($areYouSure){Remove-ItemProperty -Path $branchKey.PSPath -Name $_.Name}
+            else{Remove-ItemProperty -Path $branchKey.PSPath -Name $_.Name -WhatIf}
+            }
+        }
+    }
+
+function remove-regBranchKeyMatchingString([Microsoft.Win32.RegistryKey]$branchKey,[string]$regexExpressionString,[boolean]$areYouSure){
+    if ($(split-path $branchKey -Leaf) -match $regexExpressionString){
+        if($areYouSure){$branchKey | Remove-Item -Recurse -Force}
+        else{$branchKey | Remove-Item -Recurse -Force -WhatIf}
+        }
+    }
