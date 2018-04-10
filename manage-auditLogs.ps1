921d4ecd5627b61da3b31372ce0850fda1361d62
kevmaitland <iamkevmaitland@gmail.com>
2018-04-10 09:10:53 +0100

    _PS_Groups_Library
    Added security/unified group provisioning & management library

--- manage-auditLogs.ps1
+++ manage-auditLogs.ps1
@@ -1,12 +1,12 @@
 ﻿Import-Module _PS_Library_MSOL
 connect-ToExo
 
-$daysToLookBack = 5
+$daysToLookBack = 90
 if(!$toDate){$toDate = $(Get-Date).AddDays(1)}
 $fromDate = $toDate.AddDays(-($daysToLookBack+1))
-[array]$arrayOfUsers = @("acsmailboxaccess@anthesisgroup.com ")
+[array]$arrayOfUsers = @("troy.childs@anthesisgroup.com")
 #[array]$operations = @("PasswordLogonInitialAuthUsingPassword","UserLoggedIn")
-$title = $arrayOfUsers[0]+" "+(get-date)
+$title = "$($arrayOfUsers[0])_$(Get-Date -Format yyyy-MM-dd)"
 
 
 
@@ -25,7 +25,7 @@ function export-psobjectsToCSV($arrayOfPsobjectAuditEntries){
     $nicelyFormattedArray =@()
     $nicelyFormattedArray += $fullyMemberedObject
     $arrayOfPsobjectAuditEntries | % {$nicelyFormattedArray += $_}
-    $nicelyFormattedArray | Export-Csv -Path $env:USERPROFILE\Desktop\AuditLogs\AuditLog_$($arrayOfUsers[0])_$(Get-Date -Format yyyy-MM-dd).csv -NoClobber -NoTypeInformation
+    $nicelyFormattedArray | Export-Csv -Path $env:USERPROFILE\Desktop\AuditLogs\AuditLog_$title.csv -NoClobber -NoTypeInformation
     }
 function parse-unifiedAuditLogToPsObjects($auditLogEntries){
     $auditLogEntries | %{
