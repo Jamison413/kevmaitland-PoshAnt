202681e6a704ee3b204f3b5a0aa8a8437ff9f958
Kev Maitland <iamkevmaitland@gmail.com>
2019-03-07 12:02:51 +0000

    Bitlocker Key to AAD

---
+++ add-emailtoBlacklist.ps1
@@ -0,0 +1,16 @@
+﻿
+
+[array]$newChumps = convertTo-arrayOfEmailAddresses "contact@ceooffice.info"
+
+$blackListTheseChumpsRuleName = "Blacklist these chumps"
+$blackListRepliesToTheseChumpsRuleName = "Blacklist replies to these chumps"
+
+$blackListTheseChumpsRule = Get-TransportRule | Where-Object {$_.Identity -contains $blackListTheseChumpsRuleName}
+$blackListRepliesToTheseChumpsRule = Get-TransportRule | Where-Object {$_.Identity -contains $blackListRepliesToTheseChumpsRuleName}
+
+$newChumps | % {
+    $blackListTheseChumpsRule.From.Add($_)
+    $blackListRepliesToTheseChumpsRule.SentTo.Add($_)
+    }
+$blackListTheseChumpsRule | Set-TransportRule -From $blackListTheseChumpsRule.From
+$blackListTheseChumpsRuleName | Set-TransportRule -SentTo $blackListRepliesToTheseChumpsRule.SentTo
---
+++ register-bitlockerRecoveryKeyInAad.ps1
@@ -0,0 +1,21 @@
+﻿$logFileLocation = "$env:USERPROFILE\"
+$transcriptLogName = "$($logFileLocation+$(split-path $PSCommandPath -Leaf))_Transcript_$(Get-Date -Format "yyMMdd").log"
+if ([string]::IsNullOrEmpty($MyInvocation.ScriptName)){
+    $fullLogPathAndName = $logFileLocation+"register-bitlockerRecoveryKeyInAad_FullLog_$(Get-Date -Format "yyMMdd").log"
+    $errorLogPathAndName = $logFileLocation+"register-bitlockerRecoveryKeyInAad_ErrorLog_$(Get-Date -Format "yyMMdd").log"
+    }
+else{
+    $fullLogPathAndName = "$($logFileLocation+$MyInvocation.MyCommand)_FullLog_$(Get-Date -Format "yyMMdd").log"
+    $errorLogPathAndName = "$($logFileLocation+$MyInvocation.MyCommand)_ErrorLog_$(Get-Date -Format "yyMMdd").log"
+    }
+Start-Transcript $transcriptLogName -Append
+
+$bitlockerVolume = Get-BitLockerVolume | ? {$_.ProtectionStatus -eq "on"} 
+$bitlockerVolume | % {
+    rv latestRecoveryKey -ErrorAction SilentlyContinue
+    $_.KeyProtector | %{if($_.KeyProtectorType -eq "RecoveryPassword"){$latestRecoveryKey = $_}}
+    if($latestRecoveryKey){
+        BackupToAAD-BitLockerKeyProtector -MountPoint $_.MountPoint -KeyProtectorId $latestRecoveryKey.KeyProtectorId
+        }
+    }
+Stop-Transcript
