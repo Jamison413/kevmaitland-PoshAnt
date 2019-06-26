83082ece8313b5f9d7d0a8bfedc2a58125166702
epressey <epressey@users.noreply.github.com>
2019-06-26 16:00:08 +0100

    Update report-MFAStatus.ps1

--- report-MFAStatus.ps1
+++ report-MFAStatus.ps1
@@ -19,31 +19,69 @@ $adminCreds = New-Object -TypeName System.Management.Automation.PSCredential -Ar
 
 connect-ToMsol -credential $adminCreds
 
-$enabledUsers = Get-MsolUser -EnabledFilter EnabledOnly -All | ? {$_.UserType -eq "Member"}
+
+#For Licensed User Accounts
+
+$enabledUsers = Get-MsolUser -EnabledFilter EnabledOnly -All | ? {$_.UserType -eq "Member"} | ? {$_.IsLicensed -eq "True"}  
 Write-Information "$($enabledUsers.Count) Enabled User accounts found"
-$enabledUsersWithoutMFA = $enabledUsers | ? {[string]::IsNullOrWhiteSpace($_.StrongAuthenticationRequirements)} | Sort-Object UsageLocation, DisplayName
+$enabledUsersWithoutMFA = $enabledUsers | ? {[string]::IsNullOrWhiteSpace($_.StrongAuthenticationRequirements)} | Sort-Object UsageLocation, DisplayName 
 Write-Information "$($enabledUsersWithoutMFA.Count) Enabled User accounts without MFA enabled found"
 $enabledUsersWithMFA = $enabledUsers | ? {![string]::IsNullOrWhiteSpace($_.StrongAuthenticationRequirements)}
 $suboptimalEnabledUsersWithMFA = $enabledUsersWithMFA | ? {"PhoneAppNotification" -ne $($_.StrongAuthenticationMethods | ? {$_.IsDefault -eq $true}).MethodType} | Sort-Object UsageLocation, DisplayName
 Write-Information "$($suboptimalEnabledUsersWithMFA.Count) Suboptimally configured User accounts with MFA enabled found"
+$optimalEnabledUsersWithMFA = $enabledUsersWithMFA | ? {"PhoneAppNotification" -eq $($_.StrongAuthenticationMethods | ? {$_.IsDefault -eq $true}).MethodType} | Sort-Object UsageLocation, DisplayName
+Write-Information "$($OptimalEnabledUsersWithMFA.Count) Optimally configured User accounts with MFA enabled found"
+
+#User MFA Stats
+
+[INT]$TotalMFACount = $enabledUsers.Count
+[INT]$TotalEnabledCount = $enabledUsersWithMFA.Count
+#Extra details
+    [INT]$Totalsuboptimal = $suboptimalEnabledUsersWithMFA.Count
+    [INT]$Totaloptimal = $optimalEnabledUsersWithMFA.Count
+[INT]$TotalWithoutMFA = $enabledUsersWithoutMFA.Count
+#Find the total percentage
+[INT]$TotalMFAScore = $TotalEnabledCount/$TotalMFACount*100
+[INT]$TotalOptimalMFAScore = $Totaloptimal/$TotalEnabledCount*100
+
+Write-host "$TotalMFAScore%"
+Write-host "$TotalOptimalMFAScore%"
+
+
 
 
 
 $subject = "MFA status report"
 $body = "<HTML><FONT FACE=`"Calibri`">Hello IT Team,`r`n`r`n<BR><BR>"
 #$body += $ownerReport.To+"`r`n`r`n<BR><BR>"
+
 $body += "The following users do not have MFA enabled on their accounts:"
 $body += "      `r`n`t<BR><PRE>&#9;"
 $enabledUsersWithoutMFA | % {$body += "$("$($_.UsageLocation)`t$($_.UserPrincipalName) `r`n`t")"}
 $body += "</PRE>`r`n`r`n<BR>"
+
 $body += "The following users do have MFA configured without App Notifications as their default:"
 $body += "      `r`n`t<BR><PRE>&#9;"
 $suboptimalEnabledUsersWithMFA | % {$body += $("$($_.UsageLocation)`t$($_.UserPrincipalName) `r`n`t")}
 $body += "</PRE>`r`n`r`n<BR>"
+
+$body += "The following users do have MFA configured with App Notifications as their default:"
+$body += "      `r`n`t<BR><PRE>&#9;"
+$OptimalEnabledUsersWithMFA | % {$body += $("$($_.UsageLocation)`t$($_.UserPrincipalName) `r`n`t")}
+$body += "</PRE>`r`n`r`n<BR>"
+
+$body += "Current Statistics for Anthesis:"
+$body += "      `r`n`t<BR><PRE>&#9;"
+$body += "Total MFA Score: $TotalMFAScore%"
+$body += "      `r`n`t<BR><PRE>&#9;"
+$body += "Total MFA Optimal Score: $TotalOptimalMFAScore%"
+
+
 $body += "Love,`r`n`r`n<BR><BR>The Helpful MFA Robot</FONT></HTML>"
 #Send-MailMessage -To "kevin.maitland@anthesisgroup.com" -From "thehelpfulmfarobot@anthesisgroup.com" -SmtpServer "anthesisgroup-com.mail.protection.outlook.com" -Subject $subject -BodyAsHtml $body -Encoding UTF8
 Write-Information $body
-Send-MailMessage -To @("kevin.maitland@anthesisgroup.com","emily.pressey@anthesisgroup.com") -From "thehelpfulmfarobot@anthesisgroup.com" -SmtpServer "anthesisgroup-com.mail.protection.outlook.com" -Subject $subject -BodyAsHtml $body -Encoding UTF8 -Credential $adminCreds
+Send-MailMessage -To "emily.pressey@anthesisgroup.com" -From "thehelpfulmfarobot@anthesisgroup.com" -SmtpServer "anthesisgroup-com.mail.protection.outlook.com" -Subject $subject -BodyAsHtml $body -Encoding UTF8 -Credential $adminCreds
+Send-MailMessage -To "kev.maitland@anthesisgroup.com" -From "thehelpfulmfarobot@anthesisgroup.com" -SmtpServer "anthesisgroup-com.mail.protection.outlook.com" -Subject $subject -BodyAsHtml $body -Encoding UTF8 -Credential $adminCreds
 Write-Information "Message Sent (maybe)"
 #$body
 Stop-Transcript
