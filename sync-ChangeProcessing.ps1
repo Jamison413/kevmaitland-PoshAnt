19954a7f8d6ad40d2186a48286bb7ecc12d32b45
epressey <epressey@users.noreply.github.com>
2019-10-31 18:35:27 +0000

    Update sync-ChangeProcessing.ps1
    Jira updates and Mat/Pat logic

--- sync-ChangeProcessing.ps1
+++ sync-ChangeProcessing.ps1
@@ -50,7 +50,6 @@ $List = "New Starter Details"
 Connect-PnPOnline -Credentials $adminCreds -Url $SiteURL
 $context = Get-PnPContext
 
-$task = "test"
 #Get all the items
 $AllNewStartersitems = Get-PnPListItem -List $List
 
@@ -60,7 +59,9 @@ $AllNewStartersitems = Get-PnPListItem -List $List
 $NewStarterInformation = @()
 ForEach($Item in $AllNewStartersitems){
 
-    If("1" -eq $Item.FieldValues.PowershellTrigger){
+$NewStarterInformation = @()
+
+If("1" -eq $Item.FieldValues.PowershellTrigger){
             
             
             write-host "An item has been added, and needs processing! Let's send an email to IT and People Services" -ForegroundColor Yellow
@@ -70,7 +71,7 @@ ForEach($Item in $AllNewStartersitems){
             $NewStarterInformation += New-Object psobject -Property @{
 
             "Employee Preferred Name" = $($Item.FieldValues.Employee_x0020_Preferred_x0020_N); 
-            "Start Date" = $($Item.FieldValues.Proposed_x0020_Start_x0020_Date);  
+            "Start Date" = $($Item.FieldValues.StartDate);  
             "Job Title" = $($Item.FieldValues.JobTitle);
             "Line Manager" = $($Item.FieldValues.Hiring_x0020_Manager.LookupValue);
             "Primary Team" = $($Item.FieldValues.Primary_x0020_Team0.Label);
@@ -79,19 +80,12 @@ ForEach($Item in $AllNewStartersitems){
             "Starting Office" = $($Item.FieldValues.Starting_x0020_Office0.Label);
             }
 
-            #Set the fields for Jira Tickets
-            $fields = @{
-                customfield_10045 = @{
-                value = "Bristol"
-                }
-
-                
-            }
-
             $NewStarterHTML = $NewStarterInformation | ConvertTo-Html -Property "Employee Preferred Name","Start Date","Job Title","Line Manager","Primary Team","Community","Business Unit","Starting Office" -Head "<style>table, th, td {border: 1px solid;border-collapse: collapse ;padding: 5px;text-align: left;}</style>"
 
             $htmlfriendlytitle = $List -replace " ",'%20'
             $StarterItemLink = $SiteURL + "/Lists" + "/$($htmlfriendlytitle)" +  "/DispForm.aspx?" + "ID=$($Item.FieldValues.ID)"
+            #[datetime]$date = $($Item.FieldValues.StartDate)
+            #$date = $($Item.FieldValues.StartDate) -split "/"
 
             #Send an email to People Services and IT to notify of the change and to make the change 
 
@@ -112,25 +106,42 @@ ForEach($Item in $AllNewStartersitems){
             Set-PnPListItem -List $List -Identity $Item.ID -Values @{"PowershellTrigger" = "0"}
 
             #We will also try to make a Jira task for the IT Team
-            Try{ $newissue = New-JiraIssue -Project ITC -IssueType 'Task' -Summary "External site Request: $($task)" -Fields $fields}
-            Catch{
-            write-host "Woops, something went wrong creating a Jira Tsk for this new starter" -ForegroundColor Yellow
-            }
+            If("1" -eq $Item.FieldValues.JiraTaskCreated){
+            #Set the fields for Jira Ticket
+                    $fields = @{
+                    #Workplace field
+                    customfield_10045 = @{
+                    value = "Bristol, GBR"
+                    id = "10110"
+                    }
+                    #IT Team Responsible
+                    customfield_10048 = @{
+                    value = "Bristol"
+                    id = "10129"
+                    }
+                    #customfield_10010 = @{
+                    #id = "46"
+                    #issueTypeId = "10002"
+                    #}  
+                    }
+                    #Create Jira ticket, if created set JiraTaskCreated column to "0" so it doesn't re-create (finding exisitng Jira Tickets is a pain so have opted for this route instead).
+                    Write-host "Creating Jira Ticket for New Starter Request: $($Item.FieldValues.Employee_x0020_Preferred_x0020_N)" -ForegroundColor Yellow
+                    $newissue = New-JiraIssue -Project ITC -IssueType 'Service Request' -Summary "New Starter Request: $($Item.FieldValues.Employee_x0020_Preferred_x0020_N)" -Description $($StarterItemLink) -Fields $fields
 
-            }
-
-
-
-            }
+            If($newissue){
+            Set-PnPListItem -List $List -Identity $Item.ID -Values @{"JiraTaskCreated" = "0"}
+                         }
             Else{
-            
-            write-host "Looks like there are no new starters" -ForegroundColor Yellow
-
+            Write-Host "Woops, something went wrong whilst creating a Jira Ticket for New Starter Request: $($Item.FieldValues.Employee_x0020_Preferred_x0020_N)"
             }
-
+            }
+            
 
 }
-
+Else{
+write-host "Looks like there are no new starters" -ForegroundColor Yellow
+}
+}
 
 
 <#--------------Start Date Change Processing---------------#>
@@ -162,11 +173,30 @@ ForEach($Item in $AllNewStartersitems){
         #Check if there is a difference, if there $startdate variable is null, there is no change, if there is something in there, then looks like there must be a change. Set the Last start Date to the Current Start Date and amend the FlowTrigger to set of the Calendar Management Flow.
         If($Startdatecomparison){
         Write-host "There has been a change to the Start Date: '$($Item.FieldValues.Employee_x0020_Preferred_x0020_N)'" -ForegroundColor Yellow
-        Set-PnPListItem -List $List -Identity $item.ID -Values @{"Last_StartDate" = "$startdateformat"}
-        Set-PnPListItem -List $List -Identity $item.ID -Values @{"FlowTrigger" = "Change"}
-        }
 
-   }
+        #Send email letting people know
+                    $subject = "New Starters Update: The Start Date for $($Item.FieldValues.Employee_x0020_Preferred_x0020_N) has been changed!"
+            $body = "<HTML><FONT FACE=`"Calibri`">Hello People Services & IT Teams,`r`n`r`n<BR><BR>"
+            $body += "You're receiving this email as someone has changed the Start Date for a New Starter. We'll try to update the calendar entry in the New Starters and Leavers shared calendar.`r`n`r`n<BR><BR>"
+            $body += "$($Item.FieldValues.Employee_x0020_Preferred_x0020_N): From $($Item.FieldValues.Last_StartDate) to $($Item.FieldValues.StartDate).`r`n`r`n<BR><BR>"
+            $body += "You can see more information about the New Starter here: $($StarterItemLink)`r`n`r`n<BR><BR><BR><BR>"
+            $body += "Love,`r`n`r`n<BR><BR>"
+            $body += "The People Services Robot"
+
+            Send-MailMessage -To "IT_Team_GBR_365@anthesisgroup.com" -From "thehelpfulpeopleservicesrobot@anthesisgroup.com" -SmtpServer "anthesisgroup-com.mail.protection.outlook.com" -Subject $subject -BodyAsHtml $body -Encoding UTF8
+            Send-MailMessage -To "nina.cairns@anthesisgroup.com" -From "thehelpfulpeopleservicesrobot@anthesisgroup.com" -SmtpServer "anthesisgroup-com.mail.protection.outlook.com" -Subject $subject -BodyAsHtml $body -Encoding UTF8
+            Send-MailMessage -To "elle.wright@anthesisgroup.com" -From "thehelpfulpeopleservicesrobot@anthesisgroup.com" -SmtpServer "anthesisgroup-com.mail.protection.outlook.com" -Subject $subject -BodyAsHtml $body -Encoding UTF8
+            Send-MailMessage -To "wai.cheung@anthesisgroup.com" -From "thehelpfulpeopleservicesrobot@anthesisgroup.com" -SmtpServer "anthesisgroup-com.mail.protection.outlook.com" -Subject $subject -BodyAsHtml $body -Encoding UTF8
+            Send-MailMessage -To "greg.francis@anthesisgroup.com" -From "thehelpfulpeopleservicesrobot@anthesisgroup.com" -SmtpServer "anthesisgroup-com.mail.protection.outlook.com" -Subject $subject -BodyAsHtml $body -Encoding UTF8
+            Send-MailMessage -To "emily.pressey@anthesisgroup.com" -From "thehelpfulpeopleservicesrobot@anthesisgroup.com" -SmtpServer "anthesisgroup-com.mail.protection.outlook.com" -Subject $subject -BodyAsHtml $body -Encoding UTF8 
+            
+            
+            Set-PnPListItem -List $List -Identity $Item.ID -Values @{"PowershellTrigger" = "0"}
+            Set-PnPListItem -List $List -Identity $item.ID -Values @{"Last_StartDate" = "$startdateformat"}
+            Set-PnPListItem -List $List -Identity $item.ID -Values @{"FlowTrigger" = "Change"}
+         }
+
+}
 
 
 #######################################################################################
@@ -237,12 +267,38 @@ ForEach($Item in $AllLeaversitems){
             Send-MailMessage -To "emily.pressey@anthesisgroup.com" -From "thehelpfulpeopleservicesrobot@anthesisgroup.com" -SmtpServer "anthesisgroup-com.mail.protection.outlook.com" -Subject $subject -BodyAsHtml $body -Encoding UTF8 
             Set-PnPListItem -List $List -Identity $item.ID -Values @{"PowershellTrigger" = "0"}
             }
-            Else{
-            
-            write-host "Looks like there are no new leavers" -ForegroundColor Yellow
 
+            If("1" -eq $Item.FieldValues.JiraTaskCreated){
+            #Set the fields for Jira Ticket
+                    $fields = @{
+                    #Workplace field
+                    customfield_10045 = @{
+                    value = "Bristol, GBR"
+                    id = "10110"
+                    }
+                    #IT Team Responsible
+                    customfield_10048 = @{
+                    value = "Bristol"
+                    id = "10129"
+                    }
+                    }
+                    #Create Jira ticket, if created set JiraTaskCreated column to "0" so it doesn't re-create (finding exisitng Jira Tickets is a pain so have opted for this route instead).
+                    Write-host "Creating Jira Ticket for Leaver Request: $($Item.FieldValues.Employee_x0020_Name.LookupValue)" -ForegroundColor Yellow
+                    $newissue = New-JiraIssue -Project ITC -IssueType 'Service Request' -Summary "Leaver Request: $($Item.FieldValues.Employee_x0020_Name.LookupValue)" -Description "Proposed Leaving Date: $($Item.FieldValues.Proposed_x0020_Leaving_x0020_Dat)` $($LeaverItemLink)" -Fields $fields
+
+            If($newissue){
+            Write-Host "Success! Jira ticket created"
+            Set-PnPListItem -List $List -Identity $Item.ID -Values @{"JiraTaskCreated" = "0"}
+                         }
+            Else{
+            Write-Host "Woops, something went wrong whilst creating a Jira Ticket for Leaver Request: $($Item.FieldValues.Employee_x0020_Name.LookupValue)"
+            }
             }
 
+
+Else{
+write-host "Looks like there are no new leavers" -ForegroundColor Yellow
+}
 
 }
 
@@ -320,7 +376,7 @@ $AllChangersitems = Get-PnPListItem -List $List
 ForEach($item in $AllChangersitems){
 
     $htmlfriendlytitle = $List -replace " ",'%20'
-    $ChangeRequestLink = $SiteURL + "/Lists" + "/$($htmlfriendlytitle)" +  "/DispForm.aspx?" + "ID=$($Item.FieldValues.ID)"
+    $ChangeRequestLink = $SiteURL + "/Lists" + "/$($htmlfriendlytitle)" +  "/DispForm.aspx?" + "ID=$($item.FieldValues.ID)"
 
     If("1" -eq $item.FieldValues.IsDirty){
             #Send an email to People Services and IT to notify of the change and to make the change 
@@ -356,3 +412,58 @@ ForEach($item in $AllChangersitems){
 #                                                                                     #
 #######################################################################################
 
+#Set Variables to connect to Sharepoint - People Services (All) and Notify Internal Teams of a Leaver
+$SiteURL = "https://anthesisllc.sharepoint.com/teams/People_Services_Team_All_365"
+$List = "Notify of Maternity / Paternity Leave"
+
+#Connect to Sharepoint
+Connect-PnPOnline -Credentials $adminCreds -Url $SiteURL
+$context = Get-PnPContext
+
+
+#Get all the items
+$AllMatPatitems = Get-PnPListItem -List $List
+
+<#
+        #Format the relevant fields - Sharepoint gets confused with DateTime
+        [datetime]$startdateformat = $($Item.FieldValues.StartDate)
+        
+        #If there is no Last Start Date, then set the Last Start Date to the same as the current Start Date and then skip over this iteration onto the next element.
+        If(!$Item.FieldValues.Last_StartDate){
+            Set-PnPListItem -List $List -Identity $item.ID -Values @{"Last_StartDate" = "$startdateformat"}
+            Continue
+            }
+        Else{
+        #If there is a Last Start Date then compare the two and see if it is different, because this implies that the Start Date has changed.
+            [string]$Startdate = $Item.FieldValues.StartDate
+            [string]$Last_StartDate = $Item.FieldValues.Last_StartDate
+            $Startdatecomparison = (Compare-Object -ReferenceObject $Startdate -DifferenceObject $Last_StartDate)
+            }        
+        #Check if there is a difference, if there $startdate variable is null, there is no change, if there is something in there, then looks like there must be a change. Set the Last start Date to the Current Start Date and amend the FlowTrigger to set of the Calendar Management Flow.
+        If($Startdatecomparison){
+        Write-host "There has been a change to the Start Date: '$($Item.FieldValues.Employee_x0020_Preferred_x0020_N)'" -ForegroundColor Yellow
+
+        #Send email letting people know
+                    $subject = "New Starters Update: The Start Date for $($Item.FieldValues.Employee_x0020_Preferred_x0020_N) has been changed!"
+            $body = "<HTML><FONT FACE=`"Calibri`">Hello People Services & IT Teams,`r`n`r`n<BR><BR>"
+            $body += "You're receiving this email as someone has changed the Start Date for a New Starter. We'll try to update the calendar entry in the New Starters and Leavers shared calendar.`r`n`r`n<BR><BR>"
+            $body += "$($Item.FieldValues.Employee_x0020_Preferred_x0020_N): From $($Item.FieldValues.Last_StartDate) to $($Item.FieldValues.StartDate).`r`n`r`n<BR><BR>"
+            $body += "You can see more information about the New Starter here: $($StarterItemLink)`r`n`r`n<BR><BR><BR><BR>"
+            $body += "Love,`r`n`r`n<BR><BR>"
+            $body += "The People Services Robot"
+
+            Send-MailMessage -To "IT_Team_GBR_365@anthesisgroup.com" -From "thehelpfulpeopleservicesrobot@anthesisgroup.com" -SmtpServer "anthesisgroup-com.mail.protection.outlook.com" -Subject $subject -BodyAsHtml $body -Encoding UTF8
+            Send-MailMessage -To "nina.cairns@anthesisgroup.com" -From "thehelpfulpeopleservicesrobot@anthesisgroup.com" -SmtpServer "anthesisgroup-com.mail.protection.outlook.com" -Subject $subject -BodyAsHtml $body -Encoding UTF8
+            Send-MailMessage -To "elle.wright@anthesisgroup.com" -From "thehelpfulpeopleservicesrobot@anthesisgroup.com" -SmtpServer "anthesisgroup-com.mail.protection.outlook.com" -Subject $subject -BodyAsHtml $body -Encoding UTF8
+            Send-MailMessage -To "wai.cheung@anthesisgroup.com" -From "thehelpfulpeopleservicesrobot@anthesisgroup.com" -SmtpServer "anthesisgroup-com.mail.protection.outlook.com" -Subject $subject -BodyAsHtml $body -Encoding UTF8
+            Send-MailMessage -To "greg.francis@anthesisgroup.com" -From "thehelpfulpeopleservicesrobot@anthesisgroup.com" -SmtpServer "anthesisgroup-com.mail.protection.outlook.com" -Subject $subject -BodyAsHtml $body -Encoding UTF8
+            Send-MailMessage -To "emily.pressey@anthesisgroup.com" -From "thehelpfulpeopleservicesrobot@anthesisgroup.com" -SmtpServer "anthesisgroup-com.mail.protection.outlook.com" -Subject $subject -BodyAsHtml $body -Encoding UTF8 
+            
+            
+            Set-PnPListItem -List $List -Identity $Item.ID -Values @{"PowershellTrigger" = "0"}
+            Set-PnPListItem -List $List -Identity $item.ID -Values @{"Last_StartDate" = "$startdateformat"}
+            Set-PnPListItem -List $List -Identity $item.ID -Values @{"FlowTrigger" = "Change"}
+         }
+
+
+         #>
