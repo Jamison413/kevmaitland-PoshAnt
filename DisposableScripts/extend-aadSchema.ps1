0fd930a97d0a21c5c684340bb81d6bbfe3cee11a
Kev Maitland <iamkevmaitland@gmail.com>
2020-02-18 14:38:52 +0000

    invoke-graphGet
    Paging

--- DisposableScripts/extend-aadSchema.ps1
+++ DisposableScripts/extend-aadSchema.ps1
@@ -47,3 +47,42 @@ invoke-graphPost -tokenResponse $tokenResponse -graphQuery "/schemaExtensions" -
 invoke-graphPatch -tokenResponse $tokenResponse -graphQuery "/schemaExtensions/anthesisgroup_trainingRecord" -graphBodyHashtable $newSchemaDefinition3 -Verbose
 $schemaExtension = invoke-graphGet -tokenResponse $tokenResponse -graphQuery "/schemaExtensions?`$filter=id eq 'anthesisgroup_trainingRecord'" -Verbose
 $schemaExtensions = invoke-graphGet -tokenResponse $tokenResponse -graphQuery "/schemaExtensions" -Verbose
+
+$teamBotDetails = import-encryptedCsv -pathToEncryptedCsv "$env:USERPROFILE\OneDrive - Anthesis LLC\Desktop\teambotdetails.txt"
+$teamBotToken = get-graphTokenResponse -aadAppCreds $teamBotDetails
+
+$graphUsers = invoke-graphGet -tokenResponse $teamBotToken -graphQuery "/users?`$top=500" -Verbose
+$graphUsersAll2 =  invoke-graphGet -tokenResponse $teamBotToken -graphQuery "/users?`$select=businessPhones,displayName,givenName,id,jobTitle,mail,mobilePhone,officeLocation,preferredLanguage,surname,userPrincipalName,Company,companyName,Department,creationType,directReports,manager,userType,anthesisgroup_employeeInfo" 
+$graphUsers.value.Count
+$graphUsers.'@odata.nextLink'
+
+$graphUsersAll[10] | FL
+
+$kev = $graphUsersAll |  ? {$_.DisplayName -match "Kev Maitland"}
+$kev2 = $graphUsersAll2 |  ? {$_.DisplayName -match "Kev Maitland"}
+
+invoke-graphPatch -tokenResponse $teamBotToken -graphQuery "/users/$($kev.id)" -graphBodyHashtable @{
+    anthesisgroup_employeeInfo = @{
+        contractType = "Employee"
+        }
+    } -Verbose
+
+$contractors = convertTo-arrayOfEmailAddresses "Chris.Hazen@anthesisgroup.com
+DeAnn.Sarver@anthesisgroup.com
+Deby.Stabler@anthesisgroup.com
+John.Hennessey@anthesisgroup.com
+Leslie.Macdougall@anthesisgroup.com
+Matt.Dion@anthesisgroup.com
+Susan.Mazzarella@anthesisgroup.com
+Therese.Karkowski@anthesisgroup.com
+"
+$contractors | % {
+    $thisContractor = $_
+    $graphUser = $graphUsersAll2 | ? {$_.userPrincipalName -eq $thisContractor}
+    Write-Host $graphUser.displayName
+    invoke-graphPatch -tokenResponse $teamBotToken -graphQuery "/users/$($graphUser.id)" -graphBodyHashtable @{
+        anthesisgroup_employeeInfo = @{
+            contractType = "Contractor"
+            }
+        } -Verbose    
+    }
