bff196dc287fc3989470061394c5b872ebaddf5d
epressey <44612467+epressey@users.noreply.github.com>
2021-02-17 13:43:20 +0000

    Update sync-SPdatamanagers
    New groups, less code

--- sync-SPDataManagers.ps1
+++ sync-SPDataManagers.ps1
@@ -1,9 +1,9 @@
-﻿$Logname = "C:\Scripts" + "\Logs" + "\sync-SPDataManagers $(Get-Date -Format "yyMMdd").log" #Check this location before live
+﻿$Logname = "C:\Scripts" + "\Logs" + "\sync-SPDataManagers $(Get-Date -Format "yyMMdd").log"
 Start-Transcript -Path $Logname -Append
 Write-Host "Script started:" (Get-date)
 
 Import-Module _PNP_Library_SPO
-Import-Module _PS_Library_Graph.psm1
+Import-Module _PS_Library_Graph
 
 #For pnp (Graph can't manage Sharepoint groups currently)
 $sharePointAdmin = "kimblebot@anthesisgroup.com"
@@ -11,51 +11,127 @@ $sharePointAdmin = "kimblebot@anthesisgroup.com"
 $sharePointAdminPass = ConvertTo-SecureString (Get-Content "$env:USERPROFILE\Desktop\KimbleBot.txt") 
 $adminCreds = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $sharePointAdmin, $sharePointAdminPass
 
-Connect-PnPOnline -Url "https://anthesisllc.sharepoint.com/sites/TeamHub/" -Credentials $adminCreds
 
-#For Graph
-$teamBotDetails = Import-Csv "$env:USERPROFILE\Desktop\teambotdetails.txt"
-$resource = "https://graph.microsoft.com"
-$tenantId = decrypt-SecureString (ConvertTo-SecureString $teamBotDetails.TenantId)
-$clientId = decrypt-SecureString (ConvertTo-SecureString $teamBotDetails.ClientID)
-$redirect = decrypt-SecureString (ConvertTo-SecureString $teamBotDetails.Redirect)
-$secret   = decrypt-SecureString (ConvertTo-SecureString $teamBotDetails.Secret)
+$teamBotDetails = get-graphAppClientCredentials -appName TeamsBot
+$tokenResponse = get-graphTokenResponse -aadAppCreds $teamBotDetails
 
-$ReqTokenBody = @{
-    Grant_Type    = "client_credentials"
-    Scope         = "https://graph.microsoft.com/.default"
-    client_Id     = $clientID
-    Client_Secret = $secret
-    } 
-$tokenResponse = Invoke-RestMethod -Uri "https://login.microsoftonline.com/$tenantId/oauth2/v2.0/token" -Method POST -Body $ReqTokenBody
 
-#Get members of 'Data Managers - Authorised (All) from 365' and sp group 'SPDataManagers'
+#Get members of 'Data Managers - Authorised (All) from 365' and sp groups from the team hub and client hub
 $datamanagers = get-graphUsersFromGroup -tokenResponse $tokenResponse -groupId "daf56fbd-ebce-457e-a10a-4fce50a2f99c" -memberType "Members"
-$currentspdatamanagers = Get-PnPGroupMembers -Identity "SPDataManagers"
+
+#Get members of clients (unrestricted) - Modify
+$clientsModifyMembers = get-graphUsersFromGroup -tokenResponse $tokenResponse -groupId "5084cf5a-c05e-4f10-b2af-a74417beceee" -memberType "Members"
+
+#Do any cleaning as a quick bodge until NS automation
+$leavers = $clientsModifyMembers | Where-Object -Property "displayName" -Match "Ω_"
+If($leavers){
+remove-graphUsersFromGroup -tokenResponse $tokenResponse -graphGroupId "5084cf5a-c05e-4f10-b2af-a74417beceee" -memberType Members -graphUserUpns $leavers.userPrincipalName -Verbose
+$clientsModifyMembers = get-graphUsersFromGroup -tokenResponse $tokenResponse -groupId "5084cf5a-c05e-4f10-b2af-a74417beceee" -memberType "Members"
+}
+Else{
+Write-Host "Clients (unrestircted) - Modify is already clean ('ish)" -ForegroundColor Yellow
+}
+
+
+
+
+#############################
+#                           #
+#          Team Hub         #
+#                           #
+#############################
+
+Connect-PnPOnline -Url "https://anthesisllc.sharepoint.com/sites/TeamHub/" -Credentials $adminCreds
+$DataManagerSPOGroupName = "Internal - SPO Authorised Data Managers"
+$MembersSPOGroupName = "Internal - SPO Authorised TeamHub Members "
+
+$internalcurrentspdatamanagers = Get-PnPGroupMembers -Identity $DataManagerSPOGroupName
+$internalcurrentspmembers = Get-PnPGroupMembers -Identity $MembersSPOGroupName
 
 #We just compare and add/remove any new members
 
-#Add members
-$newdatamanagers = Compare-Object -ReferenceObject $datamanagers.mail -DifferenceObject $currentspdatamanagers.Email | where-object -Property "SideIndicator" -EQ "<="
+#Add managers
+$newdatamanagers = Compare-Object -ReferenceObject $datamanagers.mail -DifferenceObject $internalcurrentspdatamanagers.Email | where-object -Property "SideIndicator" -EQ "<="
 ForEach($newdatamanager in $newdatamanagers){
-Write-Host "New Data Manager: Adding $($newdatamanager.InputObject) to SPDataManagers" -ForegroundColor Yellow
-$spdatamanagers = Add-PnPUserToGroup -LoginName $($newdatamanager.InputObject) -Identity "SPDataManagers"
+Write-Host "New Data Manager: Adding $($newdatamanager.InputObject) to $($DataManagerSPOGroupName)" -ForegroundColor Yellow
+$spdatamanagers = Add-PnPUserToGroup -LoginName $($newdatamanager.InputObject) -Identity $DataManagerSPOGroupName
+}
+
+#Remove managers
+$removeddatamanagers = Compare-Object -ReferenceObject $datamanagers.mail -DifferenceObject $internalcurrentspdatamanagers.Email | where-object -Property "SideIndicator" -EQ "=>"
+$removeddatamanagers = $removeddatamanagers | Where-Object -property "inputObject" -ne "T1-Emily.Pressey@anthesisgroup.com" #this account is the Group Owner (can't add domain groups)
+ForEach($removeddatamanager in $removeddatamanagers){
+Write-Host "Removed Data Manager: Removing $($removeddatamanager.InputObject) from $($DataManagerSPOGroupName)" -ForegroundColor Yellow
+$spdatamanagers = Remove-PnPUserFromGroup -LoginName $($removeddatamanager.InputObject) -Identity $DataManagerSPOGroupName
+}
+
+#Add members
+$newmembers = Compare-Object -ReferenceObject $clientsModifyMembers.mail -DifferenceObject $internalcurrentspmembers.Email | where-object -Property "SideIndicator" -EQ "<="
+ForEach($newmember in $newmembers){
+Write-Host "New Member: Adding $($newmember.InputObject) to $($MembersSPOGroupName)" -ForegroundColor Yellow
+$spmembers = Add-PnPUserToGroup -LoginName $($newmember.InputObject) -Identity $MembersSPOGroupName
 }
 
 #Remove members
-$removeddatamanagers = Compare-Object -ReferenceObject $datamanagers.mail -DifferenceObject $currentspdatamanagers.Email | where-object -Property "SideIndicator" -EQ "=>"
+$removedmembers = Compare-Object -ReferenceObject $clientsModifyMembers.mail -DifferenceObject $internalcurrentspmembers.Email | where-object -Property "SideIndicator" -EQ "=>"
+$removedmembers = $removedmembers | Where-Object -property "inputObject" -ne "T1-Emily.Pressey@anthesisgroup.com" #this account is the Group Owner (can't add domain groups)
+ForEach($removedmember in $removedmembers){
+Write-Host "Removed Member: Removing $($removedmember.InputObject) from $($MembersSPOGroupName)" -ForegroundColor Yellow
+$spmembers = Remove-PnPUserFromGroup -LoginName $($removedmember.InputObject) -Identity $MembersSPOGroupName
+}
+
+
+
+
+
+
+#############################
+#                           #
+#        Client Hub         #
+#                           #
+#############################
+
+Connect-PnPOnline -Url "https://anthesisllc.sharepoint.com/clients/" -UseWebLogin #-Credentials $adminCreds
+$DataManagerSPOGroupName = "External - SPO Authorised Data Managers"
+$MembersSPOGroupName = "External - Authorised Client Members"
+
+$externalcurrentspdatamanagers = Get-PnPGroupMembers -Identity $DataManagerSPOGroupName
+$externalcurrentspmembers = Get-PnPGroupMembers -Identity $MembersSPOGroupName
+
+#We just compare and add/remove any new members to the two Sharepoint groups - external
+
+#Add Managers
+$newdatamanagers = Compare-Object -ReferenceObject $datamanagers.mail -DifferenceObject $externalcurrentspdatamanagers.Email | where-object -Property "SideIndicator" -EQ "<="
+ForEach($newdatamanager in $newdatamanagers){
+Write-Host "New Data Manager: Adding $($newdatamanager.InputObject) to $($DataManagerSPOGroupName)" -ForegroundColor Yellow
+$spdatamanagers = Add-PnPUserToGroup -LoginName $($newdatamanager.InputObject) -Identity $DataManagerSPOGroupName
+}
+
+#Remove Managers
+$removeddatamanagers = Compare-Object -ReferenceObject $datamanagers.mail -DifferenceObject $externalcurrentspdatamanagers.Email | where-object -Property "SideIndicator" -EQ "=>"
+$removeddatamanagers = $removeddatamanagers | Where-Object -property "inputObject" -ne "T1-Emily.Pressey@anthesisgroup.com" #this account is the Group Owner (can't add domain groups)
 ForEach($removeddatamanager in $removeddatamanagers){
 Write-Host "Removed Data Manager: Removing $($removeddatamanager.InputObject) from SPDataManagers" -ForegroundColor Yellow
-$spdatamanagers = Remove-PnPUserFromGroup -LoginName $($removeddatamanager.InputObject) -Identity "SPDataManagers"
+$spdatamanagers = Remove-PnPUserFromGroup -LoginName $($removeddatamanager.InputObject) -Identity $DataManagerSPOGroupName
+}
+
+
+#Add members
+$newmembers = Compare-Object -ReferenceObject $clientsModifyMembers.mail -DifferenceObject $externalcurrentspmembers.Email | where-object -Property "SideIndicator" -EQ "<="
+ForEach($newmember in $newmembers){
+Write-Host "New Member: Adding $($newmember.InputObject) to $($MembersSPOGroupName)" -ForegroundColor Yellow
+$spmembers = Add-PnPUserToGroup -LoginName $($newmember.InputObject) -Identity $MembersSPOGroupName
+}
+
+#Remove members
+$removedmembers = Compare-Object -ReferenceObject $clientsModifyMembers.mail -DifferenceObject $externalcurrentspmembers.Email | where-object -Property "SideIndicator" -EQ "=>"
+$removedmembers = $removedmembers | Where-Object -property "inputObject" -ne "T1-Emily.Pressey@anthesisgroup.com" #this account is the Group Owner (can't add domain groups)
+ForEach($removedmember in $removedmembers){
+Write-Host "Removed Member: Removing $($removedmember.InputObject) from $($MembersSPOGroupName)" -ForegroundColor Yellow
+$spmembers = Remove-PnPUserFromGroup -LoginName $($removedmember.InputObject) -Identity $MembersSPOGroupName
 }
 
 
 
-<#Add each member to the sp group SPDataManagers (saved for rebuilding the list if there is an issue, might save a bit of effort)
-ForEach($datamanager in $datamanagers){
-Write-Host "Adding $($datamanager.mail) to SPDataManagers" -ForegroundColor Yellow
-$spdatamanagers = Add-PnPUserToGroup -LoginName "$($datamanager.mail)" -Identity "SPDataManagers"
-}
-#>
 
 Stop-Transcript
