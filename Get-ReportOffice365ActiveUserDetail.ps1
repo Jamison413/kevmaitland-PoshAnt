10b84e53a8bc12d6bdf1cb2250f1173be1ed80d1
Andy-Ost <53262043+Andy-Ost@users.noreply.github.com>
2021-05-26 15:09:03 +0100

    Create Get-ReportOffice365ActiveUserDetail.ps1

---
+++ Get-ReportOffice365ActiveUserDetail.ps1
@@ -0,0 +1,14 @@
+﻿
+    
+$reportbotDetails = get-graphAppClientCredentials -appName ReportBot
+$tokenResponseReportBot = get-graphTokenResponse -aadAppCreds $reportbotDetails -grant_type client_credentials
+$report = invoke-graphGet -tokenResponse $tokenResponseReportBot -graphQuery "/reports/getOffice365ActiveUserDetail(period='D180')" 
+$arrayReport = ($report -split '\r?\n').Trim()
+$arrayReport  | out-file $env:TEMP\test.csv -Force utf8
+
+
+$csvReport = Import-Csv -Path $env:TEMP\test.csv 
+$csvReport | Export-Csv -Path $env:USERPROFILE\Downloads\test4.csv -NoTypeInformation -Force -Encoding UTF8
+
+
+
