27dca6daa03645d78a425f5f93cf16198d721e0b
Kev Maitland <iamkevmaitland@gmail.com>
2021-05-26 14:53:26 +0100

    ReportBot

---
+++ DisposableScripts/deploy-wvd.ps1
@@ -0,0 +1,4 @@
+﻿Install-Module Microsoft.RDInfra.RDPowerShell
+Import-Module Microsoft.RDInfra.RDPowerShell
+Add-RdsAccount  -DeploymentUrl https://rdbroker.wvd.microsoft.com
+New-RdsTenant -Name AADDS-WVD -AadTenantId a054308f-5864-479b-a71e-44ac3f05fd4f -AzureSubscriptionId 5fcade80-253b-4bda-91a6-9f1e9792bdfc
---
+++ DisposableScripts/disable-bitlockerEncryptionForRemovableDevices.ps1
@@ -0,0 +1,13 @@
+﻿if(Test-Path HKLM:\SYSTEM\CurrentControlSet\Policies\Microsoft\FVE){
+    try{
+        $RDVDenyWriteAccess = Get-ItemProperty -Path HKLM:\SYSTEM\CurrentControlSet\Policies\Microsoft\FVE -Name "RDVDenyWriteAccess" -ErrorAction Stop
+        if($RDVDenyWriteAccess.RDVDenyWriteAccess -eq 1){Set-ItemProperty -Path HKLM:\SYSTEM\CurrentControlSet\Policies\Microsoft\FVE -Name "RDVDenyWriteAccess" -Value 0}
+        }
+    catch{
+        New-ItemProperty -Path HKLM:\SYSTEM\CurrentControlSet\Policies\Microsoft\FVE -Name "RDVDenyWriteAccess" -Value 0
+        }
+    }
+else{
+    New-Item -Path HKLM:\SYSTEM\CurrentControlSet\Policies\Microsoft -Name FVE
+    New-ItemProperty -Path HKLM:\SYSTEM\CurrentControlSet\Policies\Microsoft\FVE -Name "RDVDenyWriteAccess" -Value 0
+    }
