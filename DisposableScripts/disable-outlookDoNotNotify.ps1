ef5b81a62a48e9b1886c3c066d664df600b18a9d
Kev Maitland <iamkevmaitland@gmail.com>
2021-07-14 13:50:08 +0100

    Update disable-outlookDoNotNotify.ps1

--- DisposableScripts/disable-outlookDoNotNotify.ps1
+++ DisposableScripts/disable-outlookDoNotNotify.ps1
@@ -1,13 +1,64 @@
-﻿if(Test-Path HKLM:\SYSTEM\CurrentControlSet\Policies\Microsoft\FVE){
-    try{
-        $RDVDenyWriteAccess = Get-ItemProperty -Path HKLM:\SYSTEM\CurrentControlSet\Policies\Microsoft\FVE -Name "RDVDenyWriteAccess" -ErrorAction Stop
-        if($RDVDenyWriteAccess.RDVDenyWriteAccess -eq 1){Set-ItemProperty -Path HKLM:\SYSTEM\CurrentControlSet\Policies\Microsoft\FVE -Name "RDVDenyWriteAccess" -Value 0}
+﻿function add-registryValue(){
+    [cmdletbinding()]
+        <#
+    .SYNOPSIS
+    
+
+    .DESCRIPTION
+    
+
+    .PARAMETER infile1
+    
+
+    .PARAMETER infile2
+    
+
+    .EXAMPLE
+    #>
+
+    param(
+        [parameter(Mandatory=$true)]
+            [String]$registryPath
+        ,[parameter(Mandatory=$true)]
+            [String]$registryKey
+        ,[parameter(Mandatory=$true)]
+            [String]$registryValue
+        ,[parameter(Mandatory=$true)]
+            [ValidateSet("String", "ExpandString", "Binary", "DWord", "MultiString", "QWord")] 
+            [String]$registryType
+        )
+
+    $registryPath = $registryPath.Replace("Computer\","")
+    $registryPath = $registryPath.Replace("HKEY_LOCAL_MACHINE","HKLM:")
+    $registryPath = $registryPath.Replace("HKEY_CURRENT_USER","HKCU:")
+
+    $registryPath -split "\\" | % { #Silently create any missing subkeys
+        $thisStub = $thisStub += $_+"\"
+        Write-Host $thisStub
+        if((Test-Path $thisStub) -eq $false){
+            New-Item -Path $thisStub | Out-Null
+            }
         }
-    catch{
-        New-ItemProperty -Path HKLM:\SYSTEM\CurrentControlSet\Policies\Microsoft\FVE -Name "RDVDenyWriteAccess" -Value 0
+
+    try {$existingItem = Get-ItemProperty -Path $registryPath -name $registryKey -ErrorAction SilentlyContinue}
+    catch{}
+    
+    Write-Verbose "`$registryPath = $registryPath"
+    Write-Verbose "`$registryKey = $registryKey"
+    Write-Verbose "`$registryValue = $registryValue"
+    Write-Verbose "`$registryType = $registryType"
+    Write-Verbose "`$existingItem = $existingItem"
+
+    if([string]::IsNullOrWhiteSpace($existingItem.$registryKey)){
+        Write-Verbose "Creating new Registry Value"
+        New-ItemProperty -Path $registryPath -Name $registryKey -Value $registryValue -PropertyType $registryType
         }
+    else{
+        Write-Verbose "Updating existing Registry Value"
+        Set-ItemProperty -Path $registryPath -Name $registryKey -Value $registryValue
+        }
+    
+    
     }
-else{
-    New-Item -Path HKLM:\SYSTEM\CurrentControlSet\Policies\Microsoft -Name FVE
-    New-ItemProperty -Path HKLM:\SYSTEM\CurrentControlSet\Policies\Microsoft\FVE -Name "RDVDenyWriteAccess" -Value 0
-    }
+
+add-registryValue -registryPath "HKLM:\SYSTEM\CurrentControlSet\Policies\Microsoft\FVE" -registryKey "RDVDenyWriteAccess" -registryValue 0 -registryType DWord
