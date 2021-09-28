45521a4d4460a2e55b9f9d813f29afdb95df5ad4
Kev Maitland <iamkevmaitland@gmail.com>
2021-09-28 14:21:17 +0100

    Update report-SharePointFileStorageUsage.ps1
    added logging

--- report-SharePointFileStorageUsage.ps1
+++ report-SharePointFileStorageUsage.ps1
@@ -1,12 +1,17 @@
-﻿$tokenResponseSharePointBot = get-graphTokenResponse -aadAppCreds $(get-graphAppClientCredentials -appName SharePointBot) -grant_type client_credentials
+﻿start-transcriptLog -thisScriptName "report-SharePointFileStorageUsage"
+
+$tokenResponseSharePointBot = get-graphTokenResponse -aadAppCreds $(get-graphAppClientCredentials -appName SharePointBot) -grant_type client_credentials
 $sitesToProcess = @("/clients")
 
 $sitesToProcess | % {
     $thisSite = get-graphSite -tokenResponse $tokenResponseSharePointBot -serverRelativeUrl $_
+    Write-Host "Processing Site [$($thisSite.name)][$($thisSite.id)][$($thisSite.webUrl)]"
     $theseDrives = get-graphDrives -tokenResponse $tokenResponseSharePointBot -siteGraphId $thisSite.id
     $theseDrives | % {
         $thisDrive = $_
+        Write-Host "`tProcessing Drive [$($thisDrive.name)][$($thisDrive.id)][$($thisDrive.webUrl)]"
         $theseDriveItems = get-graphDriveItems -tokenResponse $tokenResponseSharePointBot -driveGraphId $thisDrive.id -returnWhat Children -includePreviousVersions
+        Write-Host "`t`tProcessing [$($theseDriveItems.Count)] DriveItems"
         $output = @($null)*$theseDriveItems.Count
         for ($i=0;$i -lt $output.Count; $i++){
             $output[$i] = [pscustomobject][ordered]@{
@@ -26,6 +31,10 @@ $sitesToProcess | % {
             }
 
         $output | Sort-Object WebUrl | Select-Object * | Export-Csv  -Path "$env:USERPROFILE\Downloads\$($thisSite.name)_$($thisDrive.name)_$((Get-Date -f u).Split(" ")[0]).csv" -NoTypeInformation -Force
+        Write-Host "`t`tOutput written to [$("$env:USERPROFILE\Downloads\$($thisSite.name)_$($thisDrive.name)_$((Get-Date -f u).Split(" ")[0]).csv")]"
+        Write-Host
         }
     }
 
+
+Stop-Transcript
