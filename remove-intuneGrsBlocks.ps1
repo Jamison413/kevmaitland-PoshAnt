57f8cb79c7f2301e9f2b3a50ceeaafb168099256
Kev Maitland <iamkevmaitland@gmail.com>
2021-10-01 08:59:54 +0100

    Create remove-intuneGrsBlocks.ps1

---
+++ remove-intuneGrsBlocks.ps1
@@ -0,0 +1,4 @@
+﻿Get-ChildItem HKLM:\SOFTWARE\Microsoft\IntuneManagementExtension\Win32Apps\ | ? {@("HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\IntuneManagementExtension\Win32Apps\Reporting","HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\IntuneManagementExtension\Win32Apps\00000000-0000-0000-0000-000000000000" -notcontains $_.Name )} | % { 
+    Remove-Item $_.Name.Replace("HKEY_LOCAL_MACHINE","HKLM:") -Confirm:$false -Force 
+    } 
+ 
