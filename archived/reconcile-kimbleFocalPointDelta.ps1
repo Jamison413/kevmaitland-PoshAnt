c906caa82f0fead40f8fdb6b2cc3d1fcb350f2e7
Kev Maitland <iamkevmaitland@gmail.com>
2021-11-08 15:05:37 +0000

    Archiving

---
+++ archived/reconcile-kimbleFocalPointDelta.ps1
@@ -0,0 +1,147 @@
+﻿[cmdletbinding()]
+$logFileLocation = "C:\ScriptLogs\"
+$scriptName = "reconcile-kimbleFocalPointDelta"
+$fullLogPathAndName = $logFileLocation+$scriptName+"_FullLog_$(Get-Date -Format "yyMMdd").log"
+$errorLogPathAndName = $logFileLocation+$scriptName+"_ErrorLog_$(Get-Date -Format "yyMMdd").log"
+if($PSCommandPath){
+    $transcriptLogName = "$($logFileLocation+$(split-path $PSCommandPath -Leaf))_Transcript_$(Get-Date -Format "yyMMdd").log"
+    Start-Transcript $transcriptLogName -Append
+    }
+
+$verboseLogging = $true
+
+Import-Module _PS_Library_GeneralFunctionality
+Import-Module _PS_Library_Databases
+Import-Module _REST_Library-Kimble
+
+
+#Get set up
+$sqlDbConn = connect-toSqlServer -SQLServer "sql.sustain.co.uk" -SQLDBName "SUSTAIN_LIVE"
+$kimbleCreds = Import-Csv "$env:USERPROFILE\Desktop\Kimble.txt"
+$standardKimbleHeaders = get-kimbleHeaders -clientId $kimbleCreds.clientId -clientSecret $kimbleCreds.clientSecret -username $kimbleCreds.username -password $kimbleCreds.password -securityToken $kimbleCreds.securityToken -connectToLiveContext $true -verboseLogging $true
+$standardKimbleQueryUri = get-kimbleQueryUri
+
+log-action -myMessage " " -logFile $fullLogPathAndName
+log-action -myMessage "************************************************************************" -logFile $fullLogPathAndName
+log-action -myMessage "New $scriptName cycle" -logFile $fullLogPathAndName
+log-action -myMessage "************************************************************************" -logFile $fullLogPathAndName
+
+#region Get all modified Accounts records since the last update (delta update) - this will necessarily include all created records
+$lastCreatedAccountInDbSql = "SELECT MAX(CreatedDate) AS LastCreatedDate FROM SUS_Kimble_Accounts"
+$lastCreatedAccountInDb = Execute-SQLQueryOnSQLDB -query $lastCreatedAccountInDbSql -queryType Scalar -sqlServerConnection $sqlDbConn
+$lastModifiedAccountInDbSql = "SELECT MAX(LastModifiedDate) AS LastModifiedDate FROM SUS_Kimble_Accounts"
+$lastModifiedAccountInDb = Execute-SQLQueryOnSQLDB -query $lastModifiedAccountInDbSql -queryType Scalar -sqlServerConnection $sqlDbConn
+$cutoffModifiedAccountDate = Get-Date $lastModifiedAccountInDb -Format s #Does this need to account for Daylight Saving Time?
+
+$modifiedKimbleAccounts = get-allKimbleAccounts -pQueryUri $standardKimbleQueryUri -pRestHeaders $standardKimbleHeaders -pWhereStatement "WHERE LastModifiedDate > $cutoffModifiedAccountDate`Z" -verboseLogging $true
+log-action -myMessage "[$($modifiedKimbleAccounts.Count)] Kimble Accounts require updating" -logFile $fullLogPathAndName
+$modifiedKimbleAccounts | % {
+    if((Get-Date $_.CreatedDate) -gt $lastCreatedAccountInDb){
+        #Create any new Accounts
+        Write-Verbose "Creating Accounts [$($_.Name)]:[$($_.Id)]"
+        $result = add-kimbleAccountToFocalPointCache -kimbleAccount $_ -dbConnection $sqlDbConn
+        if($result -ne 1){[array]$duffCreateAccounts += @($_,$result)}
+        }
+    else{
+        #If it's not new, it must have been modified, so Update it
+        Write-Verbose "Updating Accounts [$($_.Name)]:[$($_.Id)]"
+        $result = update-kimbleAccountToFocalPointCache -kimbleAccount $_ -dbConnection $sqlDbConn
+        if($result -ne 1){[array]$duffModifyAccounts += @($_,$result)}
+        }
+    }
+if($modifiedKimbleAccounts){log-result -myMessage "[$($duffCreateAccounts.Count)] Accounts failed to create" -logFile $fullLogPathAndName}
+if($duffCreateAccounts){log-result $($duffCreateAccounts | % {"["+$_[0].Id+"]["+$_[0].Name+"]["+$_[1]+"];"}) -logFile $fullLogPathAndName}
+if($modifiedKimbleAccounts){log-result -myMessage "[$($duffModifyAccounts.Count)] Accounts failed to update" -logFile $fullLogPathAndName}
+if($duffModifyAccounts){log-result $($duffModifyAccounts | % {"["+$_[0].Id+"]["+$_[0].Name+"]["+$_[1]+"];"}) -logFile $fullLogPathAndName}
+#endregion
+
+#region Now do the same for Opportunities
+$lastModifiedOppInDbSql = "SELECT MAX(LastModifiedDate) AS LastModifiedDate FROM SUS_Kimble_Opps"
+$lastModifiedOppInDb = Execute-SQLQueryOnSQLDB -query $lastModifiedOppInDbSql -queryType Scalar -sqlServerConnection $sqlDbConn
+$cutoffModifiedOppDate = Get-Date $lastModifiedOppInDb -Format s #Does this need to account for Daylight Saving Time?
+$lastCreatedOppInDbSql = "SELECT MAX(CreatedDate) AS LastCreatedDate FROM SUS_Kimble_Opps"
+$lastCreatedOppInDb = Execute-SQLQueryOnSQLDB -query $lastCreatedOppInDbSql -queryType Scalar -sqlServerConnection $sqlDbConn
+
+$modifiedKimbleOpps = get-allKimbleLeads -pQueryUri $standardKimbleQueryUri -pRestHeaders $standardKimbleHeaders -pWhereStatement "WHERE LastModifiedDate > $cutoffModifiedOppDate`Z"
+log-action -myMessage "[$($modifiedKimbleProps.Count)] Kimble Opportunities require updating" -logFile $fullLogPathAndName
+$modifiedKimbleOpps | % {
+    if((Get-Date $_.CreatedDate) -gt $lastCreatedOppInDb){
+        #Create any new Opps
+        Write-Verbose "Creating Opportunity [$($_.Name)]:[$($_.Id)]"
+        $result = add-kimbleOppToFocalPointCache -kimbleOpp $_ -dbConnection $sqlDbConn
+        if ($result -ne 1){[array]$duffCreateOpps += @($_,$result)}
+        }
+    else{
+        #If it's not new, it must have been modified, so Update it
+        Write-Verbose "Updating Opportunity [$($_.Name)]:[$($_.Id)]"
+        $result = update-kimbleOppToFocalPointCache -kimbleOpp $_ -dbConnection $sqlDbConn
+        if ($result -ne 1){[array]$duffModifyOpps += @($_,$result)}
+        }
+    }
+if($modifiedKimbleOpps){log-result -myMessage "[$($duffCreateOpps.Count)] Opportunities failed to create" -logFile $fullLogPathAndName}
+if($duffCreateOpps){log-result $($duffCreateOpps | % {"["+$_[0].Id+"]["+$_[0].Name+"]["+$_[1]+"];"}) -logFile $fullLogPathAndName}
+if($modifiedKimbleOpps){log-result -myMessage "[$($duffModifyOpps.Count)] Opportunities failed to update" -logFile $fullLogPathAndName}
+if($duffModifyOpps){log-result $($duffModifyOpps | % {"["+$_[0].Id+"]["+$_[0].Name+"]["+$_[1]+"];"}) -logFile $fullLogPathAndName}
+#endregion
+
+#region Now do the same for Proposals
+$lastModifiedPropInDbSql = "SELECT MAX(LastModifiedDate) AS LastModifiedDate FROM SUS_Kimble_Proposals"
+$lastModifiedPropInDb = Execute-SQLQueryOnSQLDB -query $lastModifiedPropInDbSql -queryType Scalar -sqlServerConnection $sqlDbConn
+$cutoffModifiedPropDate = Get-Date $lastModifiedPropInDb -Format s #Does this need to account for Daylight Saving Time?
+$lastCreatedPropInDbSql = "SELECT MAX(CreatedDate) AS LastCreatedDate FROM SUS_Kimble_Proposals"
+$lastCreatedPropInDb = Execute-SQLQueryOnSQLDB -query $lastCreatedPropInDbSql -queryType Scalar -sqlServerConnection $sqlDbConn
+
+$modifiedKimbleProps = get-allKimbleProposals -pQueryUri $standardKimbleQueryUri -pRestHeaders $standardKimbleHeaders -pWhereStatement "WHERE LastModifiedDate > $cutoffModifiedPropDate`Z" -verboseLogging $true
+log-action -myMessage "[$($modifiedKimbleProps.Count)] Kimble Proposals require updating" -logFile $fullLogPathAndName
+$modifiedKimbleProps | % {
+    if((Get-Date $_.CreatedDate) -gt $lastCreatedPropInDb){
+        #Create any new Props
+        Write-Verbose "Creating Proposal [$($_.Name)]:[$($_.Id)]"
+        $result = add-kimbleProposalToFocalPointCache -kimbleProp $_ -dbConnection $sqlDbConn -verboseLogging $verboseLogging
+        if ($result -ne 1){[array]$duffCreateProps += @($_,$result)}
+        }
+    else{
+        #If it's not new, it must have been modified, so Update it
+        Write-Verbose "Updating Proposal [$($_.Name)]:[$($_.Id)]"
+        $result = update-kimbleProposalToFocalPointCache -kimbleProp $_ -dbConnection $sqlDbConn -verboseLogging $verboseLogging
+        if ($result -ne 1){[array]$duffModifyProps += @($_,$result)}
+        }
+    }
+if($modifiedKimbleProps){log-result -myMessage "[$($duffCreateProps.Count)] Proposals failed to create" -logFile $fullLogPathAndName}
+if($duffCreateProps){log-result $($duffCreateProps | % {"["+$_[0].Id+"]["+$_[0].Name+"]["+$_[1]+"];"}) -logFile $fullLogPathAndName}
+if($modifiedKimbleProps){log-result -myMessage "[$($duffModifyProps.Count)] Proposals failed to update" -logFile $fullLogPathAndName}
+if($duffModifyProps){log-result $($duffModifyProps | % {"["+$_[0].Id+"]["+$_[0].Name+"]["+$_[1]+"];"}) -logFile $fullLogPathAndName}
+#endregion
+
+#region Now do the same for Engagements
+$lastCreatedEngagementInDbSql = "SELECT MAX(CreatedDate) AS LastCreatedDate FROM SUS_Kimble_Engagements"
+$lastCreatedEngagementInDb = Execute-SQLQueryOnSQLDB -query $lastCreatedEngagementInDbSql -queryType Scalar -sqlServerConnection $sqlDbConn
+$lastModifiedEngagementInDbSql = "SELECT MAX(LastModifiedDate) AS LastModifiedDate FROM SUS_Kimble_Engagements"
+$lastModifiedEngagementInDb = Execute-SQLQueryOnSQLDB -query $lastModifiedEngagementInDbSql -queryType Scalar -sqlServerConnection $sqlDbConn
+$cutoffModifiedEngagementDate = Get-Date $lastModifiedEngagementInDb -Format s #Does this need to Engagement for Daylight Saving Time?
+
+#Get all modified Engagements records since the last update (delta update) - this will necessarily include all created records
+$modifiedKimbleEngagements = get-allKimbleEngagements -pQueryUri $standardKimbleQueryUri -pRestHeaders $standardKimbleHeaders -pWhereStatement "WHERE LastModifiedDate > $cutoffModifiedEngagementDate`Z" -verboseLogging $true
+log-action -myMessage "[$($modifiedKimbleEngagements.Count)] Kimble Engagements require updating" -logFile $fullLogPathAndName
+$modifiedKimbleEngagements | % {
+    if((Get-Date $_.CreatedDate) -gt $lastCreatedEngagementInDb){
+        #Create any new Engagements
+        Write-Verbose "Creating Engagement [$($_.Name)]:[$($_.Id)]"
+        $result = add-kimbleEngagementToFocalPointCache -kimbleEngagement $_ -dbConnection $sqlDbConn
+        if($result -ne 1){[array]$duffCreateEngagements += @($_,$result)}
+        }
+    else{
+        #If it's not new, it must have been modified, so Update it
+        Write-Verbose "Updating Engagement [$($_.Name)]:[$($_.Id)]"
+        $result = update-kimbleEngagementToFocalPointCache -kimbleEngagement $_ -dbConnection $sqlDbConn 
+        if($result -ne 1){[array]$duffModifyEngagements += ,@($_,$result)}
+        }
+    }
+if($modifiedKimbleEngagements){log-result -myMessage "[$($duffCreateEngagements.Count)] Engagements failed to create" -logFile $fullLogPathAndName}
+if($duffCreateEngagements){log-result $($duffCreateEngagements | % {"["+$_[0].Id+"]["+$_[0].Name+"]["+$_[1]+"];"}) -logFile $fullLogPathAndName}
+if($modifiedKimbleEngagements){log-result -myMessage "[$($duffModifyEngagements.Count)] Engagements failed to update" -logFile $fullLogPathAndName}
+if($duffModifyEngagements){log-result $($duffModifyEngagements | % {"["+$_[0].Id+"]["+$_[0].Name+"]["+$_[1]+"];"}) -logFile $fullLogPathAndName}
+#endregion
+
+$sqlDbConn.Close()
+Stop-Transcript
---
+++ archived/reconcile-kimbleFocalPointFull.ps1
@@ -0,0 +1,207 @@
+﻿$logFileLocation = "C:\ScriptLogs\"
+if ([string]::IsNullOrEmpty($MyInvocation.ScriptName)){
+    $fullLogPathAndName = $logFileLocation+"reconcile-kimbleFocalPoint_FullLog_$(Get-Date -Format "yyMMdd").log"
+    $errorLogPathAndName = $logFileLocation+"reconcile-kimbleFocalPoint_ErrorLog_$(Get-Date -Format "yyMMdd").log"
+    }
+else{
+    $fullLogPathAndName = "$($logFileLocation+$MyInvocation.MyCommand)_FullLog_$(Get-Date -Format "yyMMdd").log"
+    $errorLogPathAndName = "$($logFileLocation+$MyInvocation.MyCommand)_ErrorLog_$(Get-Date -Format "yyMMdd").log"
+    }
+if($PSCommandPath){
+    $transcriptLogName = "$($logFileLocation+$(split-path $PSCommandPath -Leaf))_Transcript_$(Get-Date -Format "yyMMdd").log"
+    Start-Transcript $transcriptLogName -Append
+    }
+
+Import-Module _PS_Library_GeneralFunctionality
+Import-Module _PS_Library_Databases
+Import-Module _REST_Library-Kimble.psm1
+
+#Get set up
+$sqlDbConn = connect-toSqlServer -SQLServer "sql.sustain.co.uk" -SQLDBName "SUSTAIN_LIVE"
+$kimbleCreds = Import-Csv "$env:USERPROFILE\Desktop\Kimble.txt"
+$standardKimbleHeaders = get-kimbleHeaders -clientId $kimbleCreds.clientId -clientSecret $kimbleCreds.clientSecret -username $kimbleCreds.username -password $kimbleCreds.password -securityToken $kimbleCreds.securityToken -connectToLiveContext $true -verboseLogging $true
+$standardKimbleQueryUri = get-kimbleQueryUri
+
+
+#region Accounts
+$cachedAccounts = get-allFocalPointCachedKimbleAccounts -dbConnection $sqlDbConn -pWhereStatement $null #-verboseLogging $true
+$kimbleAccounts = get-allKimbleAccounts -pQueryUri $standardKimbleQueryUri -pRestHeaders $standardKimbleHeaders #-pWhereStatement "WHERE Sync_to_FocalPoint__c = TRUE" 
+$accountsDelta = Compare-Object -ReferenceObject $kimbleAccounts -DifferenceObject $cachedAccounts -Property Id -PassThru -CaseSensitive:$false -IncludeEqual
+if(!$cachedAccounts){$accountsDelta = $kimbleAccounts}
+
+
+#Create any new (uncached) Accounts
+$accountsDelta | ? {$_.SideIndicator -eq "<=" } | % {
+    $me = $_
+    add-kimbleAccountToFocalPointCache -kimbleAccount $me -dbConnection $sqlDbConn
+    } | % {Write-Host $me.Id $me.Name}
+#Update all the Accounts based on the Kimble Data
+if($i -ne $null){rv i}
+$accountsDelta | ? {$_.SideIndicator -eq "==" } | % {
+    Write-Progress -Activity "Updating cached Kimble Accounts" -status "$($_.Name)" -percentComplete ($i / $kimbleAccounts.count * 100)
+    $thisAccount = $_
+    $result = update-kimbleAccountToFocalPointCache -kimbleAccount $thisAccount -dbConnection $sqlDbConn #| Out-Null 
+    if($result -ne 1){Write-Host "FAILED TO UPDATE [$($thisAccount.Id)] [$($thisAccount.Name)]"}
+    $i++
+    }
+#These are now missing from Kimble, so mark them as Deleted
+$accountsDelta | ? {$_.SideIndicator -eq "=>" } | % {
+    $me = $_
+    $me.IsDeleted = $true
+    $me | Add-Member -MemberType NoteProperty -Name "IsMissingFromKimble" -Value $true -Force
+    $result = update-kimbleAccountToFocalPointCache -kimbleAccount $me -dbConnection $sqlDbConn 
+    if($result -ne 1){Write-Host "FAILED TO UPDATE [$($me.Id)] [$($me.Name)]"}
+    }
+#endregion
+
+#region Opps
+$cachedOpps = get-allFocalPointCachedKimbleOpps -dbConnection $sqlDbConn -pWhereStatement $null
+$kimbleOpps = get-allKimbleLeads -pQueryUri $standardKimbleQueryUri -pRestHeaders $standardKimbleHeaders
+$oppsDelta = Compare-Object -ReferenceObject $kimbleOpps -DifferenceObject $cachedOpps -Property Id -PassThru -CaseSensitive:$false -IncludeEqual
+if(!$cachedOpps){$oppsDelta = $kimbleOpps}
+
+$oppsDelta | ? {$_.SideIndicator -eq "<="} | % {
+    $me = $_
+    add-kimbleOppToFocalPointCache -kimbleOpp $me -dbConnection $sqlDbConn
+    } | % {Write-Host $me.Id $me.Name}
+#Update all the Accounts based on the Kimble Data
+if($i -ne $null){rv i}
+$oppsDelta | ? {$_.SideIndicator -eq "=="} | % {
+    $me = $_
+    Write-Progress -Activity "Updating cached Kimble Opps" -status "$($me.Name)" -percentComplete ($i / $kimbleOpps.count * 100)
+    $result = update-kimbleOppToFocalPointCache -kimbleOpp $me -dbConnection $sqlDbConn -Verbose
+    if($result -ne 1){Write-Host "FAILED TO UPDATE [$($me.Id)] [$($me.Name)]"}
+    $i++
+    } | % {Write-Host $me.Id $me.Name}
+#These are now missing from Kimble. Not sure what to do with these...
+$oppsDelta | ? {$_.SideIndicator -eq "=>" } | % {
+    $me = $_
+    $me.IsDeleted = $true
+    update-kimbleOppToFocalPointCache -kimbleOpp $me -dbConnection $sqlDbConn
+    if($result -ne 1){Write-Host "FAILED TO UPDATE [$($me.Id)] [$($me.Name)]"}
+    }
+<# This populates the data for the first time
+if($i -ne $null){rv i}
+$kimbleOpps | %{
+    Write-Progress -Activity "Adding cached Kimble Opps" -status "$($_.Name)" -percentComplete ($i / $kimbleOpps.count * 100)
+    add-kimbleOppToFocalPointCache -kimbleOpp $_ -dbConnection $sqlDbConn | Out-Null 
+    Write-Host $_.Id $_.Name
+    $i++
+    }
+#>
+
+#endregion
+
+
+#region Props
+$cachedProps = get-allFocalPointCachedKimbleProps -dbConnection $sqlDbConn -pWhereStatement $null
+$kimbleProps = get-allKimbleProposals -pQueryUri $standardKimbleQueryUri -pRestHeaders $standardKimbleHeaders
+$propsDelta = Compare-Object -ReferenceObject $kimbleProps -DifferenceObject $cachedProps -Property Id -PassThru -CaseSensitive:$false -IncludeEqual
+if(!$cachedProps){$propsDelta = $kimbleProps}
+
+$propsToAdd = $propsDelta | ? {$_.SideIndicator -eq "<="} 
+$propsToAdd | % {
+    $me = $_
+    add-kimbleProposalToFocalPointCache -kimbleProp $me -dbConnection $sqlDbConn
+    } | % {Write-Host $me.Id $me.Name}
+#Update all the matched Accounts based on the Kimble Data
+if($i -ne $null){rv i}
+$propsToUpdate = $propsDelta | ? {$_.SideIndicator -eq "=="}
+$propsToUpdate | % {
+    $me = $_
+    Write-Progress -Activity "Updating cached Kimble Proposals" -status "$($me.Name)" -percentComplete ($i / $kimbleProps.count * 100)
+    $result = update-kimbleProposalToFocalPointCache -kimbleProp $me -dbConnection $sqlDbConn 
+    if($result -ne 1){Write-Host "FAILED TO UPDATE [$($me.Id)] [$($me.Name)]"}
+    $i++
+    }
+#These are now missing from Kimble. Not sure what to do with these...
+$propsToDelete = $propsDelta | ? {$_.SideIndicator -eq "=>" }
+$propsToDelete | % {
+    $me = $_
+    $me.IsDeleted = $true
+    $me | Add-Member -MemberType NoteProperty -Name "IsMissingFromKimble" -Value $true -Force
+    $result = update-kimbleProposalToFocalPointCache -kimbleProp $me -dbConnection $sqlDbConn
+    if($result -ne 1){Write-Host "FAILED TO UPDATE [$($me.Id)] [$($me.Name)]"}
+    }
+<# This populates the data for the first time
+if($i -ne $null){rv i}
+$kimbleOpps | %{
+    Write-Progress -Activity "Adding cached Kimble Opps" -status "$($_.Name)" -percentComplete ($i / $kimbleOpps.count * 100)
+    add-kimbleOppToFocalPointCache -kimbleOpp $_ -dbConnection $sqlDbConn | Out-Null 
+    Write-Host $_.Id $_.Name
+    $i++
+    }
+#>
+
+#endregion
+
+
+
+#region Engagements
+$cachedEngagements = get-allFocalPointCachedKimbleEngagements -dbConnection $sqlDbConn -pWhereStatement $null
+$kimbleEngagements = get-allKimbleEngagements -pQueryUri $standardKimbleQueryUri -pRestHeaders $standardKimbleHeaders -pWhereStatement $null
+$engagementsDelta = Compare-Object -ReferenceObject $kimbleEngagements -DifferenceObject $cachedEngagements -Property Id -PassThru -CaseSensitive:$false -IncludeEqual
+
+$engagementsToAdd = $engagementsDelta | ? {$_.SideIndicator -eq "<="} 
+$engagementsToAdd | % {
+    $me = $_
+    add-kimbleEngagementToFocalPointCache -kimbleEngagement $me -dbConnection $sqlDbConn 
+    } | % {Write-Host $me.Id $me.Name}
+#Update all the matched Accounts based on the Kimble Data
+$engagementsToUpdate = $engagementsDelta | ? {$_.SideIndicator -eq "=="}
+if($i -ne $null){rv i}
+$failed = @()
+$engagementsToUpdate | % {
+    $me = $_
+    Write-Progress -Activity "Updating cached Kimble Engagments" -status "$($me.Name)" -percentComplete ($i / $engagementsToUpdate.count * 100)
+    $result = update-kimbleEngagementToFocalPointCache -kimbleEngagement $me -dbConnection $sqlDbConn 
+    if($result -ne 1){Write-Host "FAILED TO UPDATE [$($me.Id)] [$($me.Name)]";$failed += $me}
+    $i++
+    }
+#These are now missing from Kimble. Not sure what to do with these...
+$engagementsToDelete = $engagementsDelta | ? {$_.SideIndicator -eq "=>" }
+$engagementsToDelete | % {
+    $me = $_
+    $me.IsDeleted = $true
+    $me | Add-Member -MemberType NoteProperty -Name "IsMissingFromKimble" -Value $true -Force
+    $result = update-kimbleEngagementToFocalPointCache -kimbleEngagement $me -dbConnection $sqlDbConn
+    if($result -ne 1){Write-Host "FAILED TO UPDATE [$($me.Id)] [$($me.Name)]"}
+    }
+
+#endregion
+
+#region Contacts
+$cachedContacts = get-allFocalPointCachedKimbleContacts -dbConnection $sqlDbConn -pWhereStatement $null
+$kimbleContacts = get-allKimbleContacts -pQueryUri $standardKimbleQueryUri -pRestHeaders $standardKimbleHeaders -pWhereStatement $null
+$contactsDelta = Compare-Object -ReferenceObject $kimbleContacts -DifferenceObject $cachedContacts -Property Id -PassThru -CaseSensitive:$false -IncludeEqual
+
+$contactsToAdd = $contactsDelta | ? {$_.SideIndicator -eq "<="} 
+$contactsToAdd | % {
+    $me = $_
+    add-kimbleContactToFocalPointCache -kimbleContact $me -dbConnection $sqlDbConn 
+    } | % {Write-Host $me.Id $me.Name}
+#Update all the matched Accounts based on the Kimble Data
+$contactsToUpdate = $contactsDelta | ? {$_.SideIndicator -eq "=="}
+if($i -ne $null){rv i}
+$failed = @()
+$contactsToUpdate | % {
+    $me = $_
+    Write-Progress -Activity "Updating cached Kimble Engagments" -status "$($me.Name)" -percentComplete ($i / $engagementsToUpdate.count * 100)
+    $result = update-kimbleContactToFocalPointCache -kimbleContact $me -dbConnection $sqlDbConn 
+    if($result -ne 1){Write-Host "FAILED TO UPDATE [$($me.Id)] [$($me.Name)]";$failed += $me}
+    $i++
+    }
+#These are now missing from Kimble. Not sure what to do with these...
+$contactsToDelete = $contactsDelta | ? {$_.SideIndicator -eq "=>" }
+$contactsToDelete | % {
+    $me = $_
+    $me.IsDeleted = $true
+    $me | Add-Member -MemberType NoteProperty -Name "IsMissingFromKimble" -Value $true -Force
+    $result = update-kimbleContactToFocalPointCache -kimbleContact $me -dbConnection $sqlDbConn
+    if($result -ne 1){Write-Host "FAILED TO UPDATE [$($me.Id)] [$($me.Name)]"}
+    }
+
+#endregion
+
+$sqlDbConn.close()
+Stop-Transcript
