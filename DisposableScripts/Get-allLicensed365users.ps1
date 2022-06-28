9de8413e341aeef9498d2463ce59d0d7328ae317
Kev Maitland <iamkevmaitland@gmail.com>
2022-06-28 21:32:47 +0100

    new-InternalTeam fixes for Pnpo.Powershell

---
+++ DisposableScripts/Get-allLicensed365users.ps1
@@ -0,0 +1,19 @@
+﻿
+
+
+
+
+
+connect-ToMsol -credential $msolCredentials
+connect-ToExo -credential $msolCredentials
+connect-toAAD -credential $msolCredentials
+
+
+
+
+
+Get-MsolUser -All | Where-Object {($_.Licenses).AccountSkuId -match "EXCHANGEDESKLESS" -or ($_.Licenses).AccountSkuId -match  "ENTERPRISEPACK" -or ($_.Licenses).AccountSkuId -match "STANDARDPACK" -or ($_.Licenses).AccountSkuId -match "ENTERPRISEPREMIUM" -and 
+    $_.UserPrincipalName -notmatch "conflictminerals" -and $_.UserPrincipalName -notmatch "acsmailboxaccess" -and $_.UserPrincipalName -notmatch "SoS.test" -and $_.UserPrincipalName -notmatch "t0-" -and 
+        $_.UserPrincipalName -notmatch "t1-" -and $_.UserPrincipalName -notmatch "UKcareers" -and $_.UserPrincipalName -notmatch "Varex.PEC" -and $_.UserPrincipalName -notmatch "Barry.Holt" -and 
+        $_.UserPrincipalName -notmatch "AnthesisUKFinance" -and $_.UserPrincipalName -notmatch "Microsoft.ECM" -and $_.UserPrincipalName -notmatch "ACSSupport" -and 
+        $_.UserPrincipalName -notmatch "VarexConflictMinerals"}  | Select DisplayName, UsageLocation | Export-Csv -path C:\Users\Andrew.Ost\Desktop\CSVs\AnthesisGlobalPresence2.csv
---
+++ DisposableScripts/Get-allUnifiedGroupsVisibleinGAL.ps1
@@ -0,0 +1,5 @@
+﻿connect-ToMsol -credential $msolCredentials
+connect-ToExo -credential $msolCredentials
+
+$GroupsTohide = Get-UnifiedGroup | where {($_.DisplayName -notmatch "External - " -and $_.HiddenFromAddresslistsEnabled -eq $False)} 
+$GroupsTohide | Set-UnifiedGroup -HiddenFromAddressListsEnabled $True
