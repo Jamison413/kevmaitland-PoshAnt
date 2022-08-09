084488a670d8089b1f6c2c0ef57623b4cf99025a
Andy-Ost <53262043+Andy-Ost@users.noreply.github.com>
2022-08-09 11:27:15 +0100

    new disposables

---
+++ DisposableScripts/Get-UserEnterpriseVoiceEnabled.ps1
@@ -0,0 +1,9 @@
+﻿Import-Module -name MicrosoftTeams
+Update-Module -name MicrosoftTeams 
+Connect-MicrosoftTeams
+
+#Get all Teams users with enterprise voice enabled and spit them out to the screen so you can eyeball them
+Get-CsOnlineUser | Where-Object "EnterpriseVoiceEnabled" -eq "$True" | select displayname, enterprisevoiceenabled, hostedvoicemail, dialplan | Out-GridView
+
+#Enable Enterprise voice for the targeted user
+Set-CsPhoneNumberAssignment -Identity "lucy.dornan@anthesisgroup.com" -EnterpriseVoiceEnabled $True
