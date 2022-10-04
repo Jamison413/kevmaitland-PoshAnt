96acc1593dbbf13be246c60d5dc57117e9512370
Andy-Ost <53262043+Andy-Ost@users.noreply.github.com>
2022-10-04 15:43:06 +0100

    Just guest account

---
+++ DisposableScripts/Create-ExternalUserGuestAccount.ps1
@@ -0,0 +1,18 @@
+﻿
+#add list of upns
+$listofupns = ""
+
+#connect
+$teamBotDetails = get-graphAppClientCredentials -appName TeamsBot
+$tokenResponse = get-graphTokenResponse -aadAppCreds $teamBotDetails
+
+$formattedUpnsToInvite = convertTo-arrayOfEmailAddresses -blockOfText $listofupns
+
+#process
+ForEach($upn in $formattedUpnsToInvite){
+
+#check if email exists
+$emailFound = get-graphUsers -tokenResponse $tokenResponse -filterCustomEq @{"mail" = $upn} -Verbose}
+
+#create guest account
+$newGuestAccount = new-GraphGuestInvitation -tokenResponse $tokenResponse -invitedUserEmailAddress $upn -inviteRedirectUrl "https://myapps.microsoft.com/anthesisgroup.com" -sendInvitationMessage $false -Verbose
