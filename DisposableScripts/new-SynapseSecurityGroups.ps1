bded7d950c512b8120426a05b689929097e95916
Kev Maitland <iamkevmaitland@gmail.com>
2023-01-19 21:45:40 +0000

    Create new-SynapseSecurityGroups.ps1

---
+++ DisposableScripts/new-SynapseSecurityGroups.ps1
@@ -0,0 +1,17 @@
+﻿#https://learn.microsoft.com/en-us/azure/synapse-analytics/security/how-to-set-up-access-control
+$workspace = "NetSuiteMI_Prod"
+$synapseSgs = @(
+     "AzSynapse - $workspace - Admins"
+    ,"AzSynapse - $workspace - SqlAdmins"
+    ,"AzSynapse - $workspace - Contibutors"
+    ,"AzSynapse - $workspace - ComputeOperators"
+    ,"AzSynapse - $workspace - CredentialUsers"
+    ,"AzSynapse - $workspace - DataReaders"
+    )
+
+$groupsToken = get-graphTokenResponse -aadAppCreds $(get-graphAppClientCredentials -appName TeamAndCommunityBot)
+
+$synapseSgs | ForEach-Object {
+    new-graphGroup -tokenResponse $groupsToken -groupDisplayName $_ -groupDescription $_ -groupType Security -membershipType Assigned -groupOwners $(whoami -upn)
+}
+
